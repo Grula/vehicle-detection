ad6ac177862738bce099929f92e90b4a1103e2bc
Nikola <grula93@gmail.com>
2022-06-15 18:06:14 +0200

    Adding base nvidia tf2 implementation

---
+++ src/augmentation/stylegan2-tf-2.x/dataset.py
@@ -0,0 +1,60 @@
+import os
+import numpy as np
+import tensorflow as tf
+
+RES = (64, 64)
+
+def _parse_file(filename):
+    image_string = tf.io.read_file(filename)
+    image = tf.image.decode_jpeg(image_string, channels=3)
+
+    # image = tf.image.resize(image, RES)
+
+    image = tf.transpose(image, perm=[1, 2, 0])
+    image = tf.cast(image, tf.float32)
+    image = image / 127.5 - 1.0
+    image = tf.transpose(image, perm=[2, 0, 1])
+    return image
+
+def create_dataset(data_base_dir, batch_size, epochs=None,):
+    filenames = tf.constant(os.listdir(data_base_dir))
+    dataset = tf.data.Dataset.from_tensor_slices(filenames)
+   
+    dataset = dataset.map(_parse_file)
+    dataset = dataset.shuffle(buffer_size=1000)
+    dataset = dataset.repeat(epochs)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    
+    return dataset
+
+def _create_dataset(data_base_dir, batch_size, epochs=None, ):
+    # create absolute path
+    data_base_dir = os.path.abspath(data_base_dir)
+    return tf.keras.utils.image_dataset_from_directory(data_base_dir, batch_size = batch_size)
+
+def main():
+    from PIL import Image
+
+    res = 64
+    batch_size = 4
+    epochs = 1
+    tfrecord_dir = './tfrecords'
+
+    # dataset = get_ffhq_dataset(tfrecord_dir, res, batch_size, epochs)
+
+    # for real_images in dataset.take(4):
+    #     # real_images: [batch_size, 3, res, res] (-1.0 ~ 1.0) float32
+    #     print(real_images.shape)
+
+    #     images = real_images.numpy()
+    #     images = np.transpose(images, axes=(0, 2, 3, 1))
+    #     images = (images + 1.0) * 127.5
+    #     images = images.astype(np.uint8)
+    #     image = Image.fromarray(images[0])
+    #     image.show()
+    # return
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/augmentation/stylegan2-tf-2.x/tf_utils.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+
+from distutils.version import StrictVersion
+
+
+def allow_memory_growth():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    print("Current GPUs: ", gpus if gpus else "None")
+
+    # Preparing for future use instead of GPU
+    tpus = tf.config.experimental.list_physical_devices('TPU')
+    print("Current TPUs: ", tpus if tpus else "None")
+
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+    return
+
+
+def split_gpu_for_testing(mem_in_gb=4):
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpus[0],
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024 * mem_in_gb),
+                 tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024 * mem_in_gb)]
+            )
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(f'{len(gpus)} Physical GPUs, {len(logical_gpus)} Logical GPUs')
+        except RuntimeError as e:
+            print(e)
+    return
+

b39fb1a22b9d1bcfe659bbdf1dac3ff0aa2b9c8a
Nikola <grula93@gmail.com>
2022-07-31 19:31:04 +0200

    wip

--- src/object_detection/v4Yolo/train.py
+++ src/object_detection/v4Yolo/train.py
@@ -230,7 +230,6 @@ def create_model(input_shape, anchors_stride_base, num_classes, load_pretrained=
 
     model = Model([model_body.input, *y_true], loss_list)
     model.summary()
-    os._exit(1)
 
     return model, model_body
 

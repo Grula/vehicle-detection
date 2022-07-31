451f3daacc10cc99be102a3ebc999690f37755e5
Nikola <grula93@gmail.com>
2022-07-31 19:47:35 +0200

    wip

--- src/object_detection/v4Yolo/train.py
+++ src/object_detection/v4Yolo/train.py
@@ -76,7 +76,7 @@ def _main():
     input_shape = (608, 608) # multiple of 32, hw
 
     model, model_body = create_model(input_shape, anchors_stride_base, num_classes,
-                                    load_pretrained=True, freeze_body=1,
+                                    load_pretrained=False, freeze_body=2,
                                     weights_path=weights_path)
 
     logging = TensorBoard(log_dir=log_dir)
@@ -190,7 +190,7 @@ def create_model(input_shape, anchors_stride_base, num_classes, load_pretrained=
                                       'anchors': anchors_stride_base})([*model_body.output, *y_true])
 
     model = Model([model_body.input, *y_true], loss_list)
-    model.summary()
+    # model.summary()
 
     return model, model_body
 

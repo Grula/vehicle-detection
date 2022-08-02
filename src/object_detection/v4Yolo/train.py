1c2b317128fcac88efec9e2934620221429e937c
Nikola <grula93@gmail.com>
2022-08-02 14:45:51 +0200

    debug testing

--- src/object_detection/v4Yolo/train.py
+++ src/object_detection/v4Yolo/train.py
@@ -120,16 +120,14 @@ def _main():
     if True:
         model.compile(optimizer=adam_v2.Adam(learning_rate=1e-3), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
         batch_size = 8
-        epochs = 5
         # DEBUG START
         # using just for testing
         # batch_size = 8
-        epochs = 1
         # DEBUG END
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit(data_generator_wrapper(lines_train, batch_size, anchors_stride_base, num_classes, max_bbox_per_scale, 'train'),
                 steps_per_epoch=max(1, num_train//batch_size),
-                epochs=epochs,
+                epochs=1,
                 initial_epoch=0,
                 callbacks=[logging, checkpoint])
 

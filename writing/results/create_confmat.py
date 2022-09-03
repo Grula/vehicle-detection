961da93ff3759ba2575cf4a26dc1fb46ddf9775c
Nikola <grula93@gmail.com>
2022-09-03 17:01:37 +0200

    tweaks

--- writing/results/create_confmat.py
+++ writing/results/create_confmat.py
@@ -1,4 +1,5 @@
 
+from ast import Gt
 import os
 import numpy as np
 from sklearn import metrics
@@ -56,8 +57,11 @@ for folder in folders:
     if len(csv_files) == 0:
         print("No .csv file found in this folder")
         continue
-    
     for csvf in csv_files:
+
+        if 'NO' not in csvf:
+            continue
+        
         print("###################################################")
         print("Current file: ", csvf)
         file_path = os.path.join(folder, csvf)
@@ -67,47 +71,86 @@ for folder in folders:
         lines = list(map(lambda x: x.strip().split(','), lines))
         data.close()
 
+        print(len(lines))
+
+        _dict_data = {}
+        for line in lines:
+            if line[0] not in _dict_data:
+                _dict_data[line[0]] = []
+            _dict_data[line[0]].append(line[1:])
+
+  
+  
+        score_threshold = 0.5
+
+        filter_score_data = {}
+        for img_path, img_data in _dict_data.items():
+            # filter low scores
+            for _d in img_data:
+                score = float(_d[2])
+                if score >= score_threshold:
+                    if img_path not in filter_score_data:
+                        filter_score_data[img_path] = []
+                    filter_score_data[img_path].append(_d)
+        print("Number of images: ", len(filter_score_data))
 
         confusion_matrix = {'car': {'car': 0, 'motorbike': 0, 'bus': 0, 'truck': 0},
-                            'motorbike': {'car': 0, 'motorbike': 0, 'bus': 0, 'truck': 0},
-                            'bus': {'car': 0, 'motorbike': 0, 'bus': 0, 'truck': 0}, 
-                            'truck': {'car': 0, 'motorbike': 0, 'bus': 0, 'truck': 0}
-                        }
+                                    'motorbike': {'car': 0, 'motorbike': 0, 'bus': 0, 'truck': 0},
+                                    'bus': {'car': 0, 'motorbike': 0, 'bus': 0, 'truck': 0}, 
+                                    'truck': {'car': 0, 'motorbike': 0, 'bus': 0, 'truck': 0}
+                                    }
 
-
+        confusion_matrix = np.zeros( (len(classes)+1, len(classes))+1 )
+        filter_iou_data = {}
         iou_threshold = 0.5
-        n = 0
-        for line in lines:
-            line = list(map(lambda x: x.strip(), line))
-            # get ground truth and prediction
-            gt = None if line[-2] == 'None' else line[-2]
-            pr = None if line[-1] == 'None' else line[-1]
+        for img_path, img_data in filter_score_data.items():
+            # calculate iou
+            for _d in img_data:
+                gt = _d[-2]
+                pred = _d[-1]
 
-            if gt == None or pr == None:
-                iou_current = 0
-            else:
-                gt = list(map(int, line[-2].split(' ')))
-                pr = list(map(int, line[-1].split(' ')))
-                # calculate current IOU
-                iou_current = intersection_over_union(gt, pr)
+                gt_box = [int(float(x)) for x in gt.strip().split(' ')]
+                pred_box = [int(float(x)) for x in pred.strip().split(' ')]
+                iou_current = intersection_over_union(gt_box, pred_box)
+                if iou_current >= iou_threshold:
+                    if img_path not in filter_iou_data:
+                        filter_iou_data[img_path] = {'true_label': _d[0], 'pred_label': _d[1], 'iou_score':iou_current, 'gt': gt, 'pred': pred}
+                    else:
+                        if filter_iou_data[img_path]['iou_score'] < iou_current:
+                            filter_iou_data[img_path] = {'true_label': _d[0], 'pred_label': _d[1], 'iou_score':iou_current, 'gt': gt, 'pred': pred}
+        print("Number of images after iou filter: ", len(filter_iou_data))
+
+        # for line in lines:
+            # line = list(map(lambda x: x.strip(), line))
+            # # get ground truth and prediction
+            # gt = None if line[-2] == 'None' else line[-2]
+            # pr = None if line[-1] == 'None' else line[-1]
+
+            # if gt == None or pr == None:
+            #     iou_current = 0
+            # else:
+            #     gt = list(map(int, line[-2].split(' ')))
+            #     pr = list(map(int, line[-1].split(' ')))
+            #     # calculate current IOU
+            #     iou_current = intersection_over_union(gt, pr)
 
             
-            # if IoU is greater than threshold we count it as true positive
+            # # if IoU is greater than threshold we count it as true positive
 
-            # check if label is predicted correctly
-            true_label =  None if line[1] == 'None' else line[1]
-            pred_label =  None if line[2] == 'None' else line[2]
+            # # check if label is predicted correctly
+            # true_label =  None if line[1] == 'None' else line[1]
+            # pred_label =  None if line[2] == 'None' else line[2]
             
             
-            # The model does not predict the label and is not a part of the ground truth.
-            if true_label is None or pred_label is None:
-                continue
+            # # The model does not predict the label and is not a part of the ground truth.
+            # if true_label is None or pred_label is None:
+            #     continue
 
-            if true_label == pred_label:
-                if iou_current > iou_threshold:
-                    confusion_matrix[true_label][pred_label] += 1
-            else:
-                confusion_matrix[true_label][pred_label] += 1
+            # if true_label == pred_label:
+            #     if iou_current > iou_threshold:
+            #         confusion_matrix[true_label][pred_label] += 1
+            # else:
+            #     confusion_matrix[true_label][pred_label] += 1
 
 
 
@@ -118,3 +161,4 @@ for folder in folders:
             print("{:<10} {:<10} {:<10} {:<10} {:<10}".format(cls, confusion_matrix[cls]['car'], confusion_matrix[cls]['motorbike'], confusion_matrix[cls]['bus'], confusion_matrix[cls]['truck']))
         print("###################################################")
         print("")
+        exit()

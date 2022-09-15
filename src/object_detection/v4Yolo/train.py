2620d5450658ab856f56c09444fedb9ff1489a8f
Nikola <grula93@gmail.com>
2022-09-15 20:06:54 +0200

    param adj

--- src/object_detection/v4Yolo/train.py
+++ src/object_detection/v4Yolo/train.py
@@ -145,6 +145,16 @@ def _main():
                 initial_epoch=0,
                 callbacks=[logging, checkpoint, early_stopping_1, reduce_lr_1, stop_on_nan])
 
+    if True:
+        model.compile(optimizer=adam_v2.Adam(learning_rate=1e-3), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+        # model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=1e-1), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+        batch_size = 32 
+        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
+        model.fit(data_generator_wrapper(lines_train, batch_size, anchors_stride_base, num_classes, max_bbox_per_scale, 'train'),
+                steps_per_epoch=max(1, num_train//batch_size),
+                epochs=100,
+                initial_epoch=0,
+                callbacks=[logging, checkpoint, early_stopping_1, reduce_lr_1, stop_on_nan])
 
 
     # Unfreeze and continue training, to fine-tune.

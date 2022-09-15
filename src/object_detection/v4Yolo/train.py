a076b253c98daf1d46c982649260115e9640a94e
Nikola <grula93@gmail.com>
2022-09-15 18:11:04 +0200

    param adj

--- src/object_detection/v4Yolo/train.py
+++ src/object_detection/v4Yolo/train.py
@@ -133,7 +133,7 @@ def _main():
     # Train with frozen layers first, to get a stable loss.
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
     if True:
-        model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.5), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+        model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.8), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         batch_size = 64
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit(data_generator_wrapper(lines_train, batch_size, anchors_stride_base, num_classes, max_bbox_per_scale, 'train'),

cfc4b961d2e8fed506aff4c14ea5aba8cbecc157
Nikola <grula93@gmail.com>
2022-09-15 06:28:47 +0200

    small fix

--- src/object_detection/v4Yolo/train.py
+++ src/object_detection/v4Yolo/train.py
@@ -105,7 +105,7 @@ def _main():
     with open(annotation_train_path) as f:
         lines_train = f.readlines()
 
-    lines_train = lines_train[:10]
+    lines_train = lines_train
 
     np.random.seed(42)
     np.random.shuffle(lines_train)
@@ -131,7 +131,7 @@ def _main():
     if True:
         # model.compile(optimizer=adam_v2.Adam(learning_rate=1e-7), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
         model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
-        batch_size = 1
+        batch_size = 8
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit(data_generator_wrapper(lines_train, batch_size, anchors_stride_base, num_classes, max_bbox_per_scale, 'train'),
                 steps_per_epoch=max(1, num_train//batch_size),

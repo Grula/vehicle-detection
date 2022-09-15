f84bc933d4eae71b7f897167db1093708aea7d08
Nikola <grula93@gmail.com>
2022-09-15 23:07:15 +0200

    param adj

--- src/object_detection/v4Yolo/train.py
+++ src/object_detection/v4Yolo/train.py
@@ -93,7 +93,7 @@ def _main():
     np.random.shuffle(lines_train)
     np.random.seed(None)   
 
-    lines_train = lines_train[:10]
+    # lines_train = lines_train[:10]
 
 
 
@@ -147,7 +147,8 @@ def _main():
             decay_rate=0.9
             )
         # model.compile(optimizer=adam_v2.Adam(learning_rate=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
-        model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr_schedule), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+        # model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr_schedule), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+        model.compile(optimizer=tf.keras.optimizers.Nadam(learning_rate=lr_schedule), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         batch_size = 8 
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit(data_generator_wrapper(lines_train, batch_size, anchors_stride_base, num_classes, max_bbox_per_scale, 'train'),

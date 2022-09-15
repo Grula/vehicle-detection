44216de9124afaa4ddfdbc5d5f84ac600436cfe5
Nikola <grula93@gmail.com>
2022-09-15 21:30:17 +0200

    param adj

--- src/object_detection/v4Yolo/train.py
+++ src/object_detection/v4Yolo/train.py
@@ -162,7 +162,7 @@ def _main():
             decay_steps=1e-3/100,
             decay_rate=0.9
             )
-        model.compile(optimizer=adam_v2.Adam(learning_rate=1e-3), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+        model.compile(optimizer=adam_v2.Adam(learning_rate=lr_schedule), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         # model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=1e-1), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         batch_size = 16 
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
@@ -170,7 +170,7 @@ def _main():
                 steps_per_epoch=max(1, num_train//batch_size),
                 epochs=100,
                 initial_epoch=0,
-                callbacks=[logging, checkpoint, early_stopping_1, reduce_lr_1, stop_on_nan])
+                callbacks=[logging, checkpoint, early_stopping_1, stop_on_nan])
 
 
     # Unfreeze and continue training, to fine-tune.
@@ -188,7 +188,7 @@ def _main():
             epochs=200,
             initial_epoch=0,
             # callbacks=[logging, checkpoint, reduce_lr, early_stopping])
-            callbacks=[logging, checkpoint, reduce_lr, early_stopping_2, evaluation])
+            callbacks=[logging, checkpoint, reduce_lr, early_stopping_2, evaluation, stop_on_nan])
 
     # Further training if needed.
 
